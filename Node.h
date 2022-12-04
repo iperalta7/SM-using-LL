dc5939f84ba72c3880394abfb68910371125efb7
Isaiah Peralta <98777083+iperalta7@users.noreply.github.com>
2022-12-04 14:45:32 -0500

    add data members for

--- Node.h
+++ Node.h
@@ -1,1 +1,24 @@
 
+#ifndef NODE_H
+#define NODE_H
+
+//class for Nodes
+// points to the node in the row
+// points to the node in the next col
+// holds the position using a row, col value
+// also holds the
+
+class Node {
+public:
+    int data;
+    int row;
+    int column;
+    Node *next_row;
+    Node *next_col;
+    Node();//default constructor
+    friend class Matrix;
+};
+
+
+#endif//NODE_H
+
