573e8a6671b098517c8cced5ce1ff106f045926a
Isaiah Peralta <98777083+iperalta7@users.noreply.github.com>
2022-12-04 21:36:33 -0500

    init member values to matrix.h

--- Matrix.h
+++ Matrix.h
@@ -1,1 +1,21 @@
+#ifndef MATRIX_H
+#define MATRIX_H
 
+#include "Node.h"
+#include <string>
+
+class Matrix {
+public:
+        int row, col;
+        Node *first_row[100];
+        Node *first_col[100];
+
+        Matrix(); //default constructor
+
+        //Constructor to set size of matrix
+        Matrix(int row, int col);
+
+};
+
+
+#endif//MATRIX_H
